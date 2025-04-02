f0b8284828a71918216c580194169d8a145b9279
bielbarros <gabrielbarrosg11@gmail.com>
2025-04-01 22:26:25 -0300

    feat: Updating services

--- ranking-funcionarios/src/app/services/funcionario.service.ts
+++ ranking-funcionarios/src/app/services/funcionario.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Funcionario } from '../models/funcionario.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FuncionarioService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/funcionarios'; // URL da sua API
+
+  constructor(private http: HttpClient) { }
+
+  getFuncionarios(): Observable<Funcionario[]> {
+    return this.http.get<Funcionario[]>(this.apiUrl);
+  }
+
+  criarFuncionario(funcionario: Funcionario): Observable<Funcionario> {
+    return this.http.post<Funcionario>(this.apiUrl + '/criar', funcionario);
+  }
 }
