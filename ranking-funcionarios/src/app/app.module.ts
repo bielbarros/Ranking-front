7bca85ce629b7a408e75cef6424016a118413adf
bielbarros <gabrielbarrosg11@gmail.com>
2025-04-05 13:08:45 -0300

    feat: Adding routes

---
+++ ranking-funcionarios/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FuncionarioListComponent } from './components/funcionario-list/funcionario-list.component';
+import { FuncionarioCreateComponent } from './components/funcionario-create/funcionario-create.component';
+import { FuncionarioUpdateComponent } from './components/funcionario-update/funcionario-update.component';
+import { FuncionarioDeleteComponent } from './components/funcionario-delete/funcionario-delete.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,  // Adiciona o AppComponent como standalone
+  imports: [RouterModule, FuncionarioListComponent, FuncionarioCreateComponent, FuncionarioUpdateComponent, FuncionarioDeleteComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    title = 'Meu App de Funcionários'; // Defina a variável title
+  }
--- ranking-funcionarios/src/app/app.routes.ts
+++ ranking-funcionarios/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { FuncionarioListComponent } from './components/funcionario-list/funcionario-list.component';
+import { FuncionarioCreateComponent } from './components/funcionario-create/funcionario-create.component';
+import { FuncionarioUpdateComponent } from './components/funcionario-update/funcionario-update.component';
+import { FuncionarioDeleteComponent } from './components/funcionario-delete/funcionario-delete.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: FuncionarioListComponent },
+  { path: 'create', component: FuncionarioCreateComponent },
+  { path: 'update', component: FuncionarioUpdateComponent },
+  { path: 'delete', component: FuncionarioDeleteComponent }
+];
